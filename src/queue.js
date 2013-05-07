bf01aac26a4d1ed7b4c0d239997fca6c603d3063
Matthew Jaquish <mattjaq@yahoo.com>
2013-05-07 01:09:12 -0700

    Fixed spacing.

--- src/queue.js
+++ src/queue.js
@@ -5,120 +5,120 @@
  */
 define(function () {
 
-	var queue  = {};
-	var module = {};
+    var queue  = {};
+    var module = {};
 
 
-	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	// Queue mixin.
-	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Queue mixin.
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-	var asQueue = (function () {
+    var asQueue = (function () {
 
-		function initQueue(config) {
-			var qData = null;
+        function initQueue(config) {
+            var qData = null;
 
-			config = config || {};
+            config = config || {};
 
-			this.data = this.data || {};
-			qData = this.data['queue'] = {};
+            this.data = this.data || {};
+            qData = this.data['queue'] = {};
 
-			qData.queue  = config.queue  || [];
-			qData.offset = config.offset || 0;
+            qData.queue  = config.queue  || [];
+            qData.offset = config.offset || 0;
 
-			return this;
-		}
+            return this;
+        }
 
-		function peek() {
-			var qData = this.data['queue'];
-			var queue = qData.queue;
+        function peek() {
+            var qData = this.data['queue'];
+            var queue = qData.queue;
 
-			return (queue.length > 0) ? queue[qData.offset] : null ;
-		}
+            return (queue.length > 0) ? queue[qData.offset] : null ;
+        }
 
-		function add(item) {
-			var qData = this.data['queue'];
-			return qData.queue.push(item);
-		}
+        function add(item) {
+            var qData = this.data['queue'];
+            return qData.queue.push(item);
+        }
 
-		function remove() {
-			var qData = this.data['queue'];
-			var queue = qData.queue;
-			var item = null;
+        function remove() {
+            var qData = this.data['queue'];
+            var queue = qData.queue;
+            var item = null;
 
-			if ( queue.length === 0 ) {
-				return null;
-			}
+            if ( queue.length === 0 ) {
+                return null;
+            }
 
-			// Get the item at the front of the queue.
-			item = queue[qData.offset];
+            // Get the item at the front of the queue.
+            item = queue[qData.offset];
 
-			// Increment the offset and remove the excess space if necessary.
-			qData.offset += 1;
+            // Increment the offset and remove the excess space if necessary.
+            qData.offset += 1;
 
-			if ( (qData.offset * 2) >= queue.length ) {
-				qData.queue = queue.slice(qData.offset);
-				qData.offset = 0;
-			}
+            if ( (qData.offset * 2) >= queue.length ) {
+                qData.queue = queue.slice(qData.offset);
+                qData.offset = 0;
+            }
 
-			return item;
-		}
+            return item;
+        }
 
-		function getLength() {
-			var qData = this.data['queue'];
-			return qData.queue.length - qData.offset;
-		}
+        function getLength() {
+            var qData = this.data['queue'];
+            return qData.queue.length - qData.offset;
+        }
 
-		function isEmpty() {
-			return this.getLength() === 0;
-		}
+        function isEmpty() {
+            return this.getLength() === 0;
+        }
 
-		function clear() {
-			var qData = this.data['queue'];
-			qData.queue.length = 0;
-			qData.offset = 0;
-		}
+        function clear() {
+            var qData = this.data['queue'];
+            qData.queue.length = 0;
+            qData.offset = 0;
+        }
 
-		function toArray() {
-			var qData = this.data['queue'];
-			return qData.queue.slice( qData.offset );
-		}
+        function toArray() {
+            var qData = this.data['queue'];
+            return qData.queue.slice( qData.offset );
+        }
 
-		return function () {
-			this.initQueue = initQueue;
-			this.peek      = peek;
-			this.add       = add;
-			this.remove    = remove;
-			this.getLength = getLength;
-			this.isEmpty   = isEmpty;
-			this.clear     = clear;
-			this.toArray   = toArray;
-		};
-	} ());
-
-
-	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	// Prototypical object.
-	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-	asQueue.call(queue);
+        return function () {
+            this.initQueue = initQueue;
+            this.peek      = peek;
+            this.add       = add;
+            this.remove    = remove;
+            this.getLength = getLength;
+            this.isEmpty   = isEmpty;
+            this.clear     = clear;
+            this.toArray   = toArray;
+        };
+    } ());
 
 
-	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	// Factory.
-	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Prototypical object.
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-	function create(config) {
-		var obj = Object.create(queue);
+    asQueue.call(queue);
 
-		config = ( typeof config === 'object' ) ? config : null ;
 
-		if ( config !== null ) {
-			obj.initQueue(config);
-		}
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Factory.
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-		return obj;
-	}
+    function create(config) {
+        var obj = Object.create(queue);
+
+        config = ( typeof config === 'object' ) ? config : null ;
+
+        if ( config !== null ) {
+            obj.initQueue(config);
+        }
+
+        return obj;
+    }
 
 
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- src/table.js
+++ src/table.js
@@ -4,108 +4,108 @@
  * over a list of keys.
  */
 define(function () {
-	var table  = {};
-	var module = {};
+    var table  = {};
+    var module = {};
 
-	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	// Table mixin.
-	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Table mixin.
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-	var asTable = (function () {
+    var asTable = (function () {
 
-		function initTable(config) {
-			var tData = null;
-			config = config || {};
+        function initTable(config) {
+            var tData = null;
+            config = config || {};
 
-			this.data = this.data || {};
-			tData = this.data['table'] = {};
+            this.data = this.data || {};
+            tData = this.data['table'] = {};
 
-			tData.keys   = config.keys   || [];
-			tData.values = config.values || {};
+            tData.keys   = config.keys   || [];
+            tData.values = config.values || {};
 
-			return this;
-		}
+            return this;
+        }
 
-		function put(key, value) {
-			var tData = this.data['table'];
-			var keys = tData.keys;
-			var vals = tData.values;
+        function put(key, value) {
+            var tData = this.data['table'];
+            var keys = tData.keys;
+            var vals = tData.values;
 
-			if ( !vals.hasOwnProperty(key) ) {
-				keys.push(key);
-			}
+            if ( !vals.hasOwnProperty(key) ) {
+                keys.push(key);
+            }
 
-			vals[key] = value;
-		}
+            vals[key] = value;
+        }
 
-		function remove(key) {
-			var tData = this.data['table'];
-			var vals = tData.values;
-			var keys = tData.keys;
-			var removedVal;
-			var i = 0;
-			var l = 0;
+        function remove(key) {
+            var tData = this.data['table'];
+            var vals = tData.values;
+            var keys = tData.keys;
+            var removedVal;
+            var i = 0;
+            var l = 0;
 
-			if ( !vals.hasOwnProperty(key) ) { return; }
+            if ( !vals.hasOwnProperty(key) ) { return; }
 
-			removedVal = vals[key];
-			l = keys.length;
+            removedVal = vals[key];
+            l = keys.length;
 
-			for (; i < l; i += 1) {
-				if (keys[i] !== key) { continue; }
-				delete vals[key];
-				keys[i] = keys[l - 1];
-				l = l - 1;
-				keys.length = l;
-			}
+            for (; i < l; i += 1) {
+                if (keys[i] !== key) { continue; }
+                delete vals[key];
+                keys[i] = keys[l - 1];
+                l = l - 1;
+                keys.length = l;
+            }
 
-			return removedVal;
-		}
+            return removedVal;
+        }
 
-		function get(key) {
-			var tData = this.data['table'];
-			var vals = tData.values;
+        function get(key) {
+            var tData = this.data['table'];
+            var vals = tData.values;
 
-			if ( !vals.hasOwnProperty(key) ) { return; }
-			return vals[key];
-		}
+            if ( !vals.hasOwnProperty(key) ) { return; }
+            return vals[key];
+        }
 
-		function forEach(fn, context) {
-			var tData = this.data['table'];
-			var keys = tData.keys;
-			var vals = tData.values;
-			var key = null;
-			var i = 0;
-			var l = keys.length;
+        function forEach(fn, context) {
+            var tData = this.data['table'];
+            var keys = tData.keys;
+            var vals = tData.values;
+            var key = null;
+            var i = 0;
+            var l = keys.length;
 
-			for (; i < l; i += 1) {
-				key = keys[i];
-				(context) ?
-					fn.call(context, key, vals[key]) :
-					fn(key, vals[key]);
-			}
-		}
+            for (; i < l; i += 1) {
+                key = keys[i];
+                (context) ?
+                    fn.call(context, key, vals[key]) :
+                    fn(key, vals[key]);
+            }
+        }
 
-		return function () {
-			this.initTable = initTable;
-			this.put       = put;
-			this.remove    = remove;
-			this.get       = get;
-			this.forEach   = forEach;
-		};
-	} ());
-
-
-	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	// Prototypical object.
-	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-	asTable.call(table);
+        return function () {
+            this.initTable = initTable;
+            this.put       = put;
+            this.remove    = remove;
+            this.get       = get;
+            this.forEach   = forEach;
+        };
+    } ());
 
 
-	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	// Factory function, with optional config
-	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Prototypical object.
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    asTable.call(table);
+
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Factory function, with optional config
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     function create(config) {
         var obj = Object.create(table);
