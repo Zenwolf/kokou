9246e5ffaed104e81096f205204f68909e302808
Matthew Jaquish <mattjaq@gmail.com>
2013-12-08 21:03:07 -0800

    Converted to Core.

--- src/table.js
+++ src/table.js
@@ -1,88 +1,88 @@
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// Copyright 2012, 2013 Matthew Jaquish
-// Licensed under The MIT License
-// http://opensource.org/licenses/MIT
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 /**
  * A default table implementation. Provides faster iteration
  * over properties than a normal object with for-in, since this can iterate
  * over a list of keys.
  */
-define(function () {
-    var table  = {};
-    var module = {};
+core.Class('kokou.Table', {
 
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // Table mixin.
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    construct: function () {
+        this.keys = [];
+        this._data = {};
+    },
 
-    var asTable = (function () {
+    members: {
 
-        function initTable(config) {
-            config = config || {};
+        /**
+         * Put the @key {String} and @value {Any} pair into the table.
+         */
+        put: function (key, value) {
+            var data = this._data;
 
-            this.data = this.data || {};
-
-            this.data.keys   = config.keys   || [];
-            this.data.values = config.values || {};
-
-            return this;
-        }
-
-        function put(key, value) {
-            var data = this.data;
-            var keys = data.keys;
-            var vals = data.values;
-
-            if ( !vals.hasOwnProperty(key) ) {
-                keys.push(key);
+            if ( !data.hasOwnProperty(key) ) {
+                this.keys.push(key);
             }
 
-            vals[key] = value;
-        }
+            data[key] = value;
+        },
 
-        function remove(key) {
-            var data = this.data;
-            var vals = data.values;
-            var keys = data.keys;
+        /**
+         * Remove the @key {String} and its associated value from the table.
+         */
+        remove: function (key) {
+            var data = this._data;
+            var keys = this.keys;
             var removedVal;
             var i = 0;
             var l = 0;
 
-            if ( !vals.hasOwnProperty(key) ) { return; }
+            if ( !data.hasOwnProperty(key) ) {
+                return;
+            }
 
-            removedVal = vals[key];
+            removedVal = data[key];
             l = keys.length;
 
             for (; i < l; i += 1) {
-                if (keys[i] !== key) { continue; }
-                delete vals[key];
+                if (keys[i] !== key) {
+                    continue;
+                }
+
+                delete data[key];
                 keys[i] = keys[l - 1];
                 l = l - 1;
                 keys.length = l;
             }
 
             return removedVal;
-        }
+        },
 
-        function get(key) {
-            var data = this.data;
-            var vals = data.values;
+        /**
+         *
+         */
+        get: function (key) {
+            var data = this._data;
 
-            if ( !vals.hasOwnProperty(key) ) { return; }
-            return vals[key];
-        }
+            if ( !data.hasOwnProperty(key) ) {
+                return;
+            }
 
-        function clear() {
-            this.data.keys.length = 0;
-            this.data.values = {};
-        }
+            return data[key];
+        },
 
-        function forEach(fn, context) {
-            var data = this.data;
-            var keys = data.keys;
-            var vals = data.values;
+        /**
+         *
+         */
+        clear: function () {
+            kokou.List.clear(this.keys);
+            this._data = {};
+        },
+
+        /**
+         *
+         */
+        forEach: function (fn, context) {
+            var data = this._data;
+            var keys = this.keys;
             var key = null;
             var i = 0;
             var l = keys.length;
@@ -90,52 +90,9 @@ define(function () {
             for (; i < l; i += 1) {
                 key = keys[i];
                 (context) ?
-                    fn.call(context, key, vals[key]) :
-                    fn(key, vals[key]);
+                    fn.call(context, key, data[key]) :
+                    fn(key, data[key]);
             }
         }
-
-        return function () {
-            this.initTable = initTable;
-            this.put       = put;
-            this.remove    = remove;
-            this.get       = get;
-            this.clear     = clear;
-            this.forEach   = forEach;
-        };
-    } ());
-
-
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // Prototypical object.
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    asTable.call(table);
-
-
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // Factory function, with optional config
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    function create(config) {
-        var obj = Object.create(table);
-
-        config = ( typeof config === 'object' ) ? config : null ;
-
-        if ( config !== null ) {
-            obj.initTable(config);
-        }
-
-        return obj;
     }
-
-
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // Public module.
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    module.create  = create;  // factory
-    module.asTable = asTable; // mixin
-
-    return module;
 });
