0d718a57e66bece45815f1c2ba2ab9f3a956f9e9
Matthew Jaquish <mattjaq@gmail.com>
2013-12-15 00:38:49 -0800

    License and stuff.

--- source/class/Fn.js
+++ source/class/Fn.js
@@ -1,3 +1,9 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Copyright 2012, 2013 Matthew Jaquish
+Licensed under the Apache License, Version 2.0
+http://www.apache.org/licenses/LICENSE-2.0
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
 /*
  * A utility to support functional programming.
  */
--- source/class/Table.js
+++ source/class/Table.js
@@ -1,3 +1,9 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Copyright 2012, 2013 Matthew Jaquish
+Licensed under the Apache License, Version 2.0
+http://www.apache.org/licenses/LICENSE-2.0
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
 /**
  * A default table implementation. Provides faster iteration
  * over properties than a normal object with for-in, since this can iterate
